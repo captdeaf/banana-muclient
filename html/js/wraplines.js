90e6a7ba57547fb9d8e8c5662f11d2e2b7941839
Greg Millam <walker@deafcode.com>
2011-04-30 22:34:48 -0700

    More improved and better tested wrapLines

--- html/js/wraplines.js
+++ html/js/wraplines.js
@@ -1,9 +1,27 @@
+function getCharWidthOf(element) {
+  if (element.charmeasure == undefined) {
+    var x = $('<div>ABCDEFGHIJKLMNOPQRSTUVWXYZ</div>');
+    x.css({
+      visibility: 'hidden',
+      position: 'absolute',
+      width: 'auto',
+      height: 'auto'
+    });
+    element.prepend(x);
+    element.charmeasure = x;
+  }
+  // Subtract 30 for the scrollbar.
+  return Math.floor((element.width() - 30) /
+                    (element.charmeasure.width() / 26));
+}
+
 var wrapPatterns = {};
 
 function getWrapRx(width) {
   if (wrapPatterns[width]) return wrapPatterns[width];
   var x = {
-    withspace: new RegExp('^((?:(?:<[^>]+>)*(?:[^<>&]|&.*?;)(?:<[^>]+>)*){1,' + width + '})(?:&nbsp;|\\s)*([\\s\\S]+)?$'),
+    full: new RegExp('^((?:(?:<[^>]*>)*(?:[^<>&]|&[^;]*;)){1,' + width + '}(?:<[^>]*>)*)((?:&nbsp;|\\s)+)?(\\S.+)?$'),
+    withspace: new RegExp('^((?:(?:<[^>]+>)*(?:[^<>&]|&.*?;)(?:<[^>]+>)*){1,' + width + '})((?:&nbsp;|\\s))*(\\S[\\s\\S]+)?$'),
     nospace: new RegExp('^((?:(?:<[^>]+>)*(?:[^<>&]|&.*?;)(?:<[^>]+>)*){' + width + '})([\\s\S]+)?$')
   };
   wrapPatterns[width] = x;
@@ -23,13 +41,20 @@ function wrapLines(lines,width) {
     } else {
       var rest = newline;
       var m;
+      var x;
       while (rest && rest.length > 0) {
-        if (m = rest.match(rx.withspace)) {
-          newlines.push(m[1]);
-          rest = m[2];
-        } else if (m = rest.match(rx.nospace)) {
-          newlines.push(m[1]);
-          rest = m[2];
+        if (m = rest.match(rx.full)) {
+          var spaces = m[2] || '';
+          rest = m[3] || '';
+          if (spaces.length > 0 || rest.length == 0) {
+            newlines.push(m[1]);
+          } else if (x = m[1].match(/^((?:(?:<[^>]*>)*(?:[^<>&]|&[^;]*;))+)(\s|&nbsp;)(\S+)$/)) {
+            newlines.push(x[1]);
+            rest = x[3] + (m[2] || '') + (m[3] || '');
+          } else {
+            newlines.push(m[1]);
+            rest = (m[2] || '') + (m[3] || '');
+          }
         } else {
           newlines.push(rest);
           rest = '';
