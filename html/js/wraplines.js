8db26053d97e7264580b375105e1a4f1c9b1bad6
Greg Millam <walker@deafcode.com>
2011-04-30 18:24:32 -0700

    wrapLines now works much more nicely with HTML.

--- html/js/wraplines.js
+++ html/js/wraplines.js
@@ -1,39 +1,40 @@
 var wrapPatterns = {};
 
-function makeWrapRx(width) {
-  var withspace = '((?:(?:<.*?>)?(?:[^<>&]|&.*?;)(?:<.*?>)?){0,' + width + '})(?:&nbsp;|\\s+)';
-  var nospace = '((?:(?:<.*?>)?(?:[^<>&]|&.*?;)(?:<.*?>)?){' + width + '})';
-  var end = '((?:(?:<.*?>)?(?:[^<>&]|&.*?;)(?:<.*?>)?){1,' + width + '})\s*$';
-  var rx = end + '|' + withspace + '|' + nospace;
-  return new RegExp(rx,'g');
+function getWrapRx(width) {
+  if (wrapPatterns[width]) return wrapPatterns[width];
+  var x = {
+    withspace: new RegExp('^((?:(?:<[^>]+>)*(?:[^<>&]|&.*?;)(?:<[^>]+>)*){1,' + width + '})(?:&nbsp;|\\s)*([\\s\\S]+)?$'),
+    nospace: new RegExp('^((?:(?:<[^>]+>)*(?:[^<>&]|&.*?;)(?:<[^>]+>)*){' + width + '})([\\s\S]+)?$')
+  };
+  wrapPatterns[width] = x;
+  return x;
 }
 
 function wrapLines(lines,width) {
   var newlines = [];
   var rx;
   var nlines = lines.length;
-  rx = wrapPatterns[width];
-  if (rx == undefined) {
-    rx = makeWrapRx(width);
-    wrapPatterns[width] = rx;
-  }
+  rx = getWrapRx(width);
   for (var i = 0; i < nlines; i += 1) {
     var line = lines[i] || '';
     var newline = '' + line;
     if (newline == '') {
       newlines.push('');
     } else {
-      newline = newline.replace(rx,function(str,end,withspace,nospace) {
-        if (withspace) {
-          newlines.push(withspace);
-        } else if (nospace) {
-          newlines.push(nospace);
-        } else if (end) {
-          newlines.push(end);
+      var rest = newline;
+      var m;
+      while (rest && rest.length > 0) {
+        if (m = rest.match(rx.withspace)) {
+          newlines.push(m[1]);
+          rest = m[2];
+        } else if (m = rest.match(rx.nospace)) {
+          newlines.push(m[1]);
+          rest = m[2];
         } else {
-          newlines.push(str);
+          newlines.push(rest);
+          rest = '';
         }
-      });
+      }
     }
   };
   return newlines;
